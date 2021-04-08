d295f4731a860c95fa0e0e5e41e8204210f146cd
sssohoni <67366877+sssohoni@users.noreply.github.com>
2021-04-08 16:47:40 +0530

    Update app.js

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,16 +1,64 @@
 (function () {
 'use strict';
 
-angular.module('CounterApp', [])
-.controller('CounterController', CounterController);
+var BuyList = [
+  {
+    name: "Cookies",
+    quantity: "10"
+  },
+  {
+    name: "Colddrinks",
+    quantity: "5"
+  },
+  {
+    name: "Chocolate",
+    quantity: "15"
+  },
+  {
+    name: "Cake",
+    quantity: "2"
+  }
+  {
+    name: "Waferpack",
+    quantity: "3"
+  }
+];
+angular.module('ShoppingListCheckOff', [])
+.controller('ToBuyController', ToBuyController)
+.controller('AlreadyBoughtController', AlreadyBoughtController) 
+.service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
-CounterController.$inject = ['$scope'];
-function CounterController($scope) {
+ToBuyController.$inject = ['ShoppingListCheckOffService'];
+function ToBuyController(ShoppingListCheckOffService) {
+    var BuyList = this;
+    BuyList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(BuyList.itemName, BuyList.itemQuantity);
+  }
+}
 
-  $scope.showNumberOfWatchers = function () {
+AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
+function AlreadyBoughtController(ShoppingListCheckOffService) {
+    var BoughtList = this;
+    BoughtList.addItem=function (itemIndex){
+    ShoppingListCheckOffService.addItem(BoughtList.itemName,BoughtList.itemQuantity);
+}
 
+
+function ShoppingListCheckOffService() {
+  var service = this;
+  // List of shopping items
+  var items = [];
+
+  service.addItem = function (itemName, quantity) {
+    var item = {
+      name: itemName,
+      quantity: quantity
+    };
+    items.push(item);
   };
-
+  service.getItems = function () {
+    return items;
+  };
 }
 
 })();
