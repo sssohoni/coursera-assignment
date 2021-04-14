171441239192631d3fbb504c4cb2838d477fed24
sssohoni <67366877+sssohoni@users.noreply.github.com>
2021-04-14 17:05:05 +0530

    Update app.js

--- module2-solution/app.js
+++ module2-solution/app.js
@@ -1,63 +1,77 @@
 (function () {
 'use strict';
 
-var BuyList = [
-  {
-    name: "Cookies",
-    quantity: "10"
-  },
-  {
-    name: "Colddrinks",
-    quantity: "5"
-  },
-  {
-    name: "Chocolate",
-    quantity: "15"
-  },
-  {
-    name: "Cake",
-    quantity: "2"
-  }
-  {
-    name: "Waferpack",
-    quantity: "3"
-  }
-];
 angular.module('ShoppingListCheckOff', [])
 .controller('ToBuyController', ToBuyController)
-.controller('AlreadyBoughtController', AlreadyBoughtController) 
+.controller('AlreadyBoughtController', AlreadyBoughtController)
 .service('ShoppingListCheckOffService', ShoppingListCheckOffService);
 
 ToBuyController.$inject = ['ShoppingListCheckOffService'];
 function ToBuyController(ShoppingListCheckOffService) {
-    var BuyList = this;
-    BuyList.removeItem = function (itemIndex) {
-    ShoppingListCheckOffService.removeItem(BuyList.itemName, BuyList.itemQuantity);
+
+  var ToBuyList = this;
+  ToBuyList.items = ShoppingListCheckOffService.getItems();
+  ToBuyList.removeItem = function (itemIndex) {
+    ShoppingListCheckOffService.removeItem(itemIndex);
   }
+
 }
 
 AlreadyBoughtController.$inject = ['ShoppingListCheckOffService'];
 function AlreadyBoughtController(ShoppingListCheckOffService) {
-    var BoughtList = this;
-    BoughtList.addItem=function (itemIndex){
-    ShoppingListCheckOffService.addItem(BoughtList.itemName,BoughtList.itemQuantity);
+  var Boughtlist = this;
+  Boughtlist.items = ShoppingListCheckOffService.getItemsBought();
+  Boughtlist.addItem =  function (item){
+  ShoppingListCheckOffService.addItem(item.name, item.quantity);
+  }
+
 }
 
 
 function ShoppingListCheckOffService() {
   var service = this;
-  // List of shopping items
-  var items = [];
 
-  service.addItem = function (itemName, quantity) {
-    var item = {
-      name: itemName,
-      quantity: quantity
-    };
-    items.push(item);
+var ToBuy = [
+  {
+    name:"Cookies",
+    quantity:"10"
+  },
+  {
+    name:"Pastry",
+    quantity:"5"
+  },
+  {
+    name:"Chocolates",
+    quantity:"15"
+  },
+  {
+    name:"Donuts",
+    quantity:"12"
+  },
+  {
+    name:"Burgers",
+    quantity:"11"
+  }
+];
+var Bought = [];
+service.addItem = function (itemName, quantity) {
+  var item = {
+    name: itemName,
+    quantity: quantity
   };
+  Bought.push(item);
+};
+
+  service.removeItem = function (itemIndex) {
+    ToBuy.splice(itemIndex, 1);
+  };
+
   service.getItems = function () {
-    return items;
+    return ToBuy;
+  };
+
+  service.getItemsBought = function () {
+    return Bought;
   };
 }
 
